4451e2d8e430e1de5f66beb7fcb5f428e21d5d34
Saad S <ssaeed2012@fau.edu>
2018-12-11 03:46:23 -0500

    Hopefully this might help
    Rough code you might be able to use

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+import firestore from 'firebase/firestore';
+
+const settings = {timestampsInSnapshots: true};
+const config = {
+  apiKey: 'YOUR_APIKEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  databaseURL: 'YOUR_DATABASE_URL',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  ngOnInit() {
+  firebase.initializeApp(config);
+  firebase.firestore().settings(settings);
+}
+};
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular6-firestore';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,79 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { BoardsComponent } from './boards/boards.component';
+import { BoardsDetailComponent } from './boards-detail/boards-detail.component';
+import { BoardsCreateComponent } from './boards-create/boards-create.component';
+import { BoardsEditComponent } from './boards-edit/boards-edit.component';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatTableModule,
+  MatIconModule, 
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule } from "@angular/material";
+const appRoutes: Routes = [
+  {
+    path: 'boards',
+    component: BoardsComponent,
+    data: { title: 'Boards List' }
+  },
+  {
+    path: 'boards-details/:id',
+    component: BoardsDetailComponent,
+    data: { title: 'Boards Details' }
+  },
+  {
+    path: 'boards-create',
+    component: BoardsCreateComponent,
+    data: { title: 'Create Boards' }
+  },
+  {
+    path: 'boards-edit/:id',
+    component: BoardsEditComponent,
+    data: { title: 'Edit Boards' }
+  },
+  { path: '',
+    redirectTo: '/boards',
+    pathMatch: 'full'
+  }
+];
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BoardsComponent,
+    BoardsDetailComponent,
+    BoardsCreateComponent,
+    BoardsEditComponent
+  ],
+  imports: [
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+  ReactiveFormsModule,
+  BrowserAnimationsModule,
+  MatInputModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatProgressSpinnerModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule
+  ],
+  
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
